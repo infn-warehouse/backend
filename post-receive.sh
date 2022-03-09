c2c8566a400bf06a5f2736fef80b016184de9b2a
ands90 <asantangelo90@gmail.com>
2022-03-09 18:09:42 +0100

    Fixed "post-receive.sh".

--- post-receive.sh
+++ post-receive.sh
@@ -5,7 +5,7 @@ if [[ $ref =~ .*/main$ ]];
 then
 echo "Main ref received.  Deploying main branch to production..."
 pwd
-git --work-tree=$HOME/frontend --git-dir=$HOME/frontend/.git checkout -f
+git --work-tree=$HOME/backend --git-dir=$HOME/backend/.git checkout -f
 pm2 start update.sh --no-autorestart
 else
 echo "Ref $ref successfully received.  Doing nothing: only the main branch may be deployed on this server."
